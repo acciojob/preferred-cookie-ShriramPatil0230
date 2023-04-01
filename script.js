af8c1524337809c74e22c03b2b1301bc0cb39e56
Shriram Patil <shrirampatil246@gmail.com>
2023-04-01 08:01:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+var fontsize= document.getElementById("fontsize")
+var fontcolor= document.getElementById("fontcolor")
+var myCookies = document.cookie.split("; ")
+
+myCookies.map((item)=>{
+	var cookielist = item.split("=")
+})
+
+function onSubmit(event){
+	document.cookie ='fontsize=${fontsize.value}'
+	document.cookie ='fontcolor=${font color.value}'
+	  event.preventDefault();
+}
+document.cookie = 'fontcolor{}'
